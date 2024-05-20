be3d012c9af7e68648eedf615c9b2849ffa31865
Dmytrotarn <112310528+Dmytrotarn@users.noreply.github.com>
2024-05-21 00:57:15 +0300

    upd

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+const search = document.querySelector('.search-input')
+const button = document.querySelector('.search-btn')
+
+button.addEventListener('click', handleSearch)
+
+function handleSearch(e) {
+    e.preventDefault()
+    console.log(search.value);
+}
+
